5466fc6d9e6c799b678b629df7b2be2f01c2faf4
njthanhtrang <njthanhtrang@users.noreply.github.com>
2021-04-22 12:35:14 -0700

    added Post model

---
+++ models/Post.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+// create our Post model
+class Post extends Model {}
+
+// create fields/columns for Post model
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    post_url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isURL: true,
+      },
+    },
+    // foreign key to User's id column
+    user_id: {
+      type: DataTypes.INTEGER,
+    //   establish relationship between post and user by connecting
+    // primary key (id) 
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "post",
+  }
+);
+
+module.exports = Post;
